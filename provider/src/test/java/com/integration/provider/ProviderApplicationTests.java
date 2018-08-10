718543189ee0775f3788c5631b49146ecb9af1c6
Vin <wenlong.jiang@enmotech.com>
2018-08-10 20:13:57 +0800

    remove redundant import.

--- provider/src/test/java/com/integration/provider/ProviderApplicationTests.java
+++ provider/src/test/java/com/integration/provider/ProviderApplicationTests.java
@@ -1,8 +1,5 @@
 package com.integration.provider;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
