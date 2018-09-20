c3caf88b5c5e6e5f6c245df688d7fe3f6df21ffb
Vin <wenlong.jiang@enmotech.com>
2018-09-20 09:40:52 +0800

    add lombok

--- provider/src/main/java/com/integration/provider/manager/AspectManager.java
+++ provider/src/main/java/com/integration/provider/manager/AspectManager.java
@@ -2,6 +2,7 @@ package com.integration.provider.manager;
 
 import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -11,8 +12,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -22,8 +21,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  */
 @Component
 @Aspect
+@Slf4j
 public class AspectManager {
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Pointcut("execution(public * com.integration.provider.controller.*.*(..))")
     public void pointCut() {
@@ -38,42 +37,42 @@ public class AspectManager {
         }
         HttpServletRequest request = attributes.getRequest();
         // 记录下请求内容
-        logger.debug("方法执行前...");
-        logger.debug("URL : " + request.getRequestURL().toString());
-        logger.debug("HTTP_METHOD : " + request.getMethod());
-        logger.debug("IP : " + request.getRemoteAddr());
-        logger.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        logger.debug("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        log.debug("方法执行前...");
+        log.debug("URL : " + request.getRequestURL().toString());
+        log.debug("HTTP_METHOD : " + request.getMethod());
+        log.debug("IP : " + request.getRemoteAddr());
+        log.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.debug("ARGS : " + Arrays.toString(joinPoint.getArgs()));
 
     }
 
     @AfterReturning(returning = "ret", pointcut = "pointCut()")
     public void doAfterReturning(Object ret) {
         // 处理完请求，返回内容
-        logger.debug("方法的返回值 : " + ret);
+        log.debug("方法的返回值 : " + ret);
     }
 
     //后置异常通知
     @AfterThrowing("pointCut()")
     public void throwing(JoinPoint joinPoint){
-        logger.debug("方法异常时执行.....");
-        logger.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.debug("方法异常时执行.....");
+        log.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
     }
 
     //后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
     @After("pointCut()")
     public void after(JoinPoint joinPoint){
-        logger.debug("方法最后执行.....");
-        logger.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.debug("方法最后执行.....");
+        log.debug("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
     }
 
     //环绕通知,环绕增强，相当于MethodInterceptor
     @Around("pointCut()")
     public Object around(ProceedingJoinPoint pjp) {
-        logger.debug("方法环绕start.....");
+        log.debug("方法环绕start.....");
         try {
             Object o =  pjp.proceed();
-            logger.debug("方法环绕proceed，结果是 :" + o);
+            log.debug("方法环绕proceed，结果是 :" + o);
             return o;
         } catch (Throwable e) {
             e.printStackTrace();
