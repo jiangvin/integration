5b513da1d7bc431e318d05d25aca03a065c7a363
Vin <wenlong.jiang@enmotech.com>
2019-08-29 19:08:02 +0800

    取消spring boot的依赖，并且改成1.7编译
    Change-Id: I839f1c4ca8de7513fdfceb502d98a23b974a2c5f

--- agent/src/main/java/AgentApplication.java
+++ agent/src/main/java/AgentApplication.java
@@ -1,4 +1,4 @@
-import lombok.extern.slf4j.Slf4j;
+import java.util.Map;
 
 /**
  * @author 蒋文龙(Vin)
@@ -6,9 +6,12 @@ import lombok.extern.slf4j.Slf4j;
  * @description
  * @date 2019/8/29
  */
-@Slf4j
 public class AgentApplication {
     public static void main(String[] args) {
-        System.getenv().forEach((key, value) -> log.info("key: {}, value: {}", key, value));
+        System.out.println("显示所有环境变量:");
+        int index = 0;
+        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            System.out.println(String.format("%d:%s=%s", ++index, entry.getKey(), entry.getValue()));
+        }
     }
 }
