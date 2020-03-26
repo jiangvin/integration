cb0cc33d38ff27622ddd04420313ea21eb022113
Vin <wenlong.jiang@enmotech.com>
2020-03-26 16:14:44 +0800

    优化代码
    Change-Id: Id1cd69bf27daa203bda6fa1a2cb9a646c45266c2

--- util/src/main/java/com/integration/util/configuration/LanguageConfiguration.java
+++ util/src/main/java/com/integration/util/configuration/LanguageConfiguration.java
@@ -1,7 +1,5 @@
 package com.integration.util.configuration;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
