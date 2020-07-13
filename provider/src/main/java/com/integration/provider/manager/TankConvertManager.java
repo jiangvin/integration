0e9d13b0f140e18a519c9af1bfffc3bd4222dec6
Vin <wenlong.jiang@enmotech.com>
2020-07-13 15:41:54 +0800

    新增带包和压缩
    Change-Id: Icb0a639adb1ec074651e8f7f81ee37229afe28d9

--- provider/src/main/java/com/integration/provider/manager/TankConvertManager.java
+++ provider/src/main/java/com/integration/provider/manager/TankConvertManager.java
@@ -4,8 +4,11 @@ import lombok.Cleanup;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -27,6 +30,7 @@ public class TankConvertManager {
     static class JsFile {
         String path;
         String name;
+        String content;
         List<String> moreThan = new ArrayList<>();
         int weight = 0;
     }
@@ -38,9 +42,9 @@ public class TankConvertManager {
 
     private static final String IGNORE_PATH = "\\libs\\";
 
-    private static final String VERSION = "_4.js";
+    private static final String VERSION = "_40.js";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String[] jsFolders = {"js\\web", "js\\share", "js\\app"};
 
         List<JsFile> jsFiles = new ArrayList<>();
@@ -51,10 +55,58 @@ public class TankConvertManager {
 
         String html = HOME + "\\templates\\index.html";
         jsFiles.sort(Comparator.comparing(JsFile::getWeight));
-        convertHtml(html, jsFiles);
+        createMainJs(jsFiles);
+        convertHtml(html);
     }
 
-    private static void convertJsFolder(File file, List<JsFile> jsFiles) {
+    private static void createMainJs(List<JsFile> jsFiles) throws Exception {
+        //生成
+        String path = String.format("%smain%s", JS_HOME, VERSION);
+        @Cleanup FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+        for (JsFile jsFile : jsFiles) {
+            fileOutputStream.write(jsFile.content.getBytes());
+        }
+
+        //压缩
+        String command = String.format("C:\\Users\\jiangvin\\AppData\\Roaming\\npm\\uglifyjs.cmd %s -o %s -c -m", path, path);
+        log.info("exec command:{}", command);
+        log.info("result:{}", execCmd(command));
+    }
+
+    /**
+     * 执行系统命令, 返回执行结果
+     *
+     * @param cmd 需要执行的命令
+     */
+    private static String execCmd(String cmd) throws Exception {
+        StringBuilder result = new StringBuilder();
+
+
+        // 执行命令, 返回一个子进程对象（命令在子进程中执行）
+        Process process = Runtime.getRuntime().exec(cmd);
+
+        // 方法阻塞, 等待命令执行完成（成功会返回0）
+        process.waitFor();
+
+        // 获取命令执行结果, 有两个结果: 正常的输出 和 错误的输出（PS: 子进程的输出就是主进程的输入）
+        @Cleanup BufferedReader bufferIn = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+        @Cleanup BufferedReader bufferError = new BufferedReader(new InputStreamReader(process.getErrorStream(), StandardCharsets.UTF_8));
+
+        // 读取输出
+        String line;
+        while ((line = bufferIn.readLine()) != null) {
+            result.append(line).append('\n');
+        }
+        while ((line = bufferError.readLine()) != null) {
+            result.append(line).append('\n');
+        }
+
+        process.destroy();
+        // 返回执行结果
+        return result.toString();
+    }
+
+    private static void convertJsFolder(File file, List<JsFile> jsFiles) throws IOException {
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (files == null) {
@@ -94,7 +146,7 @@ public class TankConvertManager {
         }
     }
 
-    private static void convertHtml(String path, List<JsFile> jsFiles) {
+    private static void convertHtml(String path) {
         try {
             StringBuilder content = new StringBuilder();
             @Cleanup Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8);
@@ -105,10 +157,7 @@ public class TankConvertManager {
                     continue;
                 }
 
-                for (JsFile jsFile : jsFiles) {
-                    String scriptPath = jsFile.path.replace(".js", VERSION);
-                    content.append(String.format("<script src=\"%s\"></script>", scriptPath)).append("\r\n");
-                }
+                content.append(String.format("<script src=\"main%s\"></script>", VERSION)).append("\r\n");
             }
 
             @Cleanup FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
@@ -127,31 +176,25 @@ public class TankConvertManager {
         return null;
     }
 
-    private static void convertJsFile(File file, JsFile jsFile, List<JsFile> jsFiles) {
+    private static void convertJsFile(File file, JsFile jsFile, List<JsFile> jsFiles) throws IOException {
         int weight = 0;
-        try {
-            StringBuilder content = new StringBuilder();
-            @Cleanup Stream<String> stream = Files.lines(Paths.get(file.getPath()), StandardCharsets.UTF_8);
-            for (Object object : stream.toArray()) {
-                String line = (String) object;
-                if (line.startsWith("import")) {
-                    String fileName = line.replace("'", "").replace("\"", "").replace(";", "");
-                    fileName = fileName.substring(fileName.lastIndexOf("/") + 1);
-                    Integer w = getWeight(jsFiles, fileName);
-                    if (w != null) {
-                        weight = w + 1;
-                    }
-                    jsFile.moreThan.add(fileName);
-                    continue;
+        StringBuilder content = new StringBuilder();
+        @Cleanup Stream<String> stream = Files.lines(Paths.get(file.getPath()), StandardCharsets.UTF_8);
+        for (Object object : stream.toArray()) {
+            String line = (String) object;
+            if (line.startsWith("import")) {
+                String fileName = line.replace("'", "").replace("\"", "").replace(";", "");
+                fileName = fileName.substring(fileName.lastIndexOf("/") + 1);
+                Integer w = getWeight(jsFiles, fileName);
+                if (w != null) {
+                    weight = w + 1;
                 }
-                content.append(line.replace("export default class ", "class ")).append("\r\n");
+                jsFile.moreThan.add(fileName);
+                continue;
             }
-
-            @Cleanup FileOutputStream fileOutputStream = new FileOutputStream(new File(file.getPath().replace(".js", VERSION)));
-            fileOutputStream.write(content.toString().getBytes());
-        } catch (Exception e) {
-            log.error("catch error:", e);
+            content.append(line.replace("export default class ", "class ")).append("\r\n");
         }
+        jsFile.content = content.toString();
         jsFile.weight = weight;
     }
 }
